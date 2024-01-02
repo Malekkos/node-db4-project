840bd6b1d1c95618d8a89a9b0258909e0d87839a
Christian Anderson <tehninjawaffle@gmail.com>
2024-01-02 14:40:28 -0800

    Created 4 tables and 4 other tables for migrating downm

--- data/migrations/20240102223726_first_migration.js
+++ data/migrations/20240102223726_first_migration.js
@@ -2,14 +2,20 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable()
+  await knex.schema.createTable()
+  await knex.schema.createTable()
+  await knex.schema.createTable()
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists()
+  await knex.schema.dropTableIfExists()
+  await knex.schema.dropTableIfExists()
+  await knex.schema.dropTableIfExists()
 };
