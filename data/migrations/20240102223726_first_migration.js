a835a31985ee7130b9bfab1919d127bd973ec6d4
Christian Anderson <tehninjawaffle@gmail.com>
2024-01-02 14:49:28 -0800

    Began work on implementing createTables

--- data/migrations/20240102223726_first_migration.js
+++ data/migrations/20240102223726_first_migration.js
@@ -3,9 +3,22 @@
  * @returns { Promise<void> }
  */
 exports.up = async function(knex) {
-  await knex.schema.createTable()
-  await knex.schema.createTable()
-  await knex.schema.createTable()
+  await knex.schema.createTable("recipes", table => {
+    table.increments("recipe_id")
+    table.string("recipe_name").notNullable().unique()
+    table.dateTime("created_at").notNullable()
+    table.integer("step_id")
+      .unsigned()
+      .notNullable()
+      .references("step_id")
+      .inTable("steps")
+      .onDelete("RESTRICT")
+      .onUpdate("RESTRICT")
+  })
+  await knex.schema.createTable("steps", table => {
+    table.increments("step_id")
+  })
+  await knex.schema.createTable("ingredients")
   await knex.schema.createTable()
 };
 
@@ -15,7 +28,7 @@ exports.up = async function(knex) {
  */
 exports.down = async function(knex) {
   await knex.schema.dropTableIfExists()
-  await knex.schema.dropTableIfExists()
-  await knex.schema.dropTableIfExists()
-  await knex.schema.dropTableIfExists()
+  await knex.schema.dropTableIfExists("ingredients")
+  await knex.schema.dropTableIfExists("steps")
+  await knex.schema.dropTableIfExists("recipes")
 };
