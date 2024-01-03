d1edac4eacd374d8c947cb1899434fb54a40e1bc
Christian Anderson <tehninjawaffle@gmail.com>
2024-01-03 00:14:00 -0800

    can hit my endpoint

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,3 +1,16 @@
 const router = require("express").Router()
 
+router.use("*", (req, res, next) => {
+  res.json({ message: "Hello! Working good!"})
+})
+
+
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    customMessage: "Something wrong inside of the recipes-router",
+    message: err.message,
+    stack: err.stack,
+  })
+})
+
 module.exports = router
