a0b339fe5005813c1a8a249c62a40e76d12b7f3f
Christian Anderson <tehninjawaffle@gmail.com>
2024-01-03 00:10:50 -0800

    Fixed typo in npm run server, fleshed out router, server, index

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require("dotenv").config()
+
 const server = require("./api/server.js")
 
 const port = process.env.PORT || 9000
 
 server.listen(port, () => {
-  console.log9(`api running on port ${port}`)
+  console.log(`api running on port ${port}`)
 })
